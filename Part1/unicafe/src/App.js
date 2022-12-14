ec4547e3ec464a57104de08c0a87ce20fd6b345b
SimonO7 <sdragonz461@gmail.com>
2022-12-13 23:30:15 -0800

    Complete Exercise 1.7

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -2,12 +2,15 @@ import { useState } from 'react'
 
 const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
 
-const Ratings = ({goodRanks, neutralRanks, badRanks}) => {
+const Ratings = ({goodRanks, neutralRanks, badRanks, total}) => {
   return (
     <div>
-      <p>Good {goodRanks}</p>
-      <p>Neutral {neutralRanks}</p>
-      <p>Bad {badRanks}</p>
+      <p>Good: {goodRanks}</p>
+      <p>Neutral: {neutralRanks}</p>
+      <p>Bad: {badRanks}</p>
+      <p>Total: {total}</p>
+      <p>Average: {((goodRanks * 1) + (neutralRanks * 0) + (badRanks * -1)) / total}</p>
+      <p>Positive: {(goodRanks / total) * 100}%</p>
     </div>
   )
 }
@@ -17,15 +20,31 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+
+  const addGoodRank = () => {
+    setGood(good + 1)
+    setTotal(total + 1)
+  }
+
+  const addNeutralRank = () => {
+    setNeutral(neutral + 1)
+    setTotal(total + 1)
+  }
+
+  const addBadRank = () => {
+    setBad(bad + 1)
+    setTotal(total + 1)
+  }
 
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text="Good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
+      <Button handleClick={addGoodRank} text="Good" />
+      <Button handleClick={addNeutralRank} text="Neutral" />
+      <Button handleClick={addBadRank} text="Bad" />
       <h1>Statistics</h1>
-      <Ratings goodRanks={good} neutralRanks={neutral} badRanks={bad} />
+      <Ratings goodRanks={good} neutralRanks={neutral} badRanks={bad} total={total} />
     </div>
   )
 }
