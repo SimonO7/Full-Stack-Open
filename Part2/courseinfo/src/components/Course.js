5a222e1911632502a5c1bdf125a6a97078bdd03c
SimonO7 <sdragonz461@gmail.com>
2022-12-16 16:42:24 -0800

    Fix spacing

--- Part2/courseinfo/src/components/Course.js
+++ Part2/courseinfo/src/components/Course.js
@@ -1,40 +1,40 @@
 const Course = ({ course }) => {
-    return (
-      <div>
-        {course.map(course => {
-          return (
-            <div key={course.id}>
-              <Header course={course.name} />
-              <Content parts={course.parts} />
-              <Total exercises={course.parts.map(part => part.exercises)} />
-            </div>
-          )
-        })}
-      </div>
-    )
+  return (
+    <div>
+      {course.map(course => {
+        return (
+          <div key={course.id}>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total exercises={course.parts.map(part => part.exercises)} />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
   
 const Header = ({ course }) => 
-    <h2>
-      {course}
-    </h2>
+  <h2>
+    {course}
+  </h2>
   
 const Total = ({ exercises }) => {
-    return (
-      <p>
-        Total of {exercises.reduce((sum, currentValue) => sum + currentValue)} exercises
-      </p>
-    )
+  return (
+    <p>
+      Total of {exercises.reduce((sum, currentValue) => sum + currentValue)} exercises
+    </p>
+  )
 }
   
 const Part = ({ part }) => 
-    <p>
-      {part.name} {part.exercises}
-    </p>
+  <p>
+    {part.name} {part.exercises}
+  </p>
   
 const Content = ({ parts }) => 
-    <>
-      {parts.map(part => <Part key={part.id} part={part} />)}
-    </>
+  <>
+    {parts.map(part => <Part key={part.id} part={part} />)}
+  </>
 
 export default Course
