8d477fb792af43492e2ddfa39e3f69fcf40185dd
SimonO7 <sdragonz461@gmail.com>
2022-12-18 17:32:16 -0800

    Complete Exercise 2.6 Add form to handle names entry into phonebook list

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -6,19 +6,36 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleNameEntry = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addEntry = (event) => {
+    event.preventDefault()
+
+    const entry = {
+      name: newName
+    }
+    
+    setPersons(persons.concat(entry))
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addEntry}>
         <div>
-          name: <input />
+          Name: <input value={newName} onChange={handleNameEntry} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit">Add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person => <p key={person.name}>{person.name}</p>)}
+      </div>
     </div>
   )
 }
