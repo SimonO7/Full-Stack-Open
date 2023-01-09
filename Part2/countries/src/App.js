b46ef11b8ec07af358aadf50aa1350daecfd8ce6
SimonO7 <sdragonz461@gmail.com>
2023-01-08 18:10:20 -0800

    Implement form and basic searching function

--- Part2/countries/src/App.js
+++ Part2/countries/src/App.js
@@ -1,7 +1,38 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const App = () => {
+const Results = ({countries, searchQuery}) => {
+    const countriesToShow = countries.filter(country => country.name.common.toLowerCase().includes(searchQuery.toLowerCase()))
 
+    return (
+        <div>
+            {countriesToShow.map(country => <p key={country.name.common}>{country.name.common}</p>)}
+        </div>
+    )
+}
+
+const App = () => {
+    const [searchQuery, setSearchQuery] = useState("")
+    const [countries, setCountries] = useState([])
+
+    const handleSearchQuery = (event) => {
+        console.log(event)
+        setSearchQuery(event.target.value)
+    }
+
+    //Get the countries from the API
+    useEffect(() => {
+        axios.get("https://restcountries.com/v3.1/all").then(response => {
+            setCountries(response.data)
+        })
+    }, [])
+
+    return (
+       <div>
+            <div>Find countries <input value={searchQuery} onChange={handleSearchQuery}></input></div>
+            <Results countries={countries} searchQuery={searchQuery} />
+       </div> 
+    )
+    
 }
 export default App
